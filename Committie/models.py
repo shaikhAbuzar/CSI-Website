d47fcc0e06100c701432f3bbd90702f23375f61d
Abuzar  Shaikh <shaikhabuzar01@gmail.com>
2020-08-08 16:38:59 +0530

    Completed the committee model completely

--- Committie/models.py
+++ Committie/models.py
@@ -5,32 +5,31 @@ from django.db import models
 class Committie(models.Model):
 	Image = models.ImageField(upload_to='members/')
 	Name = models.CharField(max_length=50)
-
-	Quote = models.CharField(max_length=200)
-	Linkedin = models.CharField(max_length=250)
+	Linkedin = models.URLField(blank=True)
 	Category = models.CharField(max_length=10, choices=(('Faculty', 'Faculty'), ('Student', 'Student')), default='Student')
 	choices = (
+		('Incharge', 'Incharge'),
 		('Staff Coordinator', 'Staff Coordinator'),
 		('President', 'President'),
 		('Vice President', 'Vice President'),
 		('Secretary', 'Secretary'),
 		('Joint Secretary', 'Joint Secretary'),
 		('Sponsorship Head', ' Sponsorship Head'),
-		('Joint Sponsorship', 'Joint Sponsorship'),
+		('Joint Sponsorship Head', 'Joint Sponsorship Head'),
 		('PR Head', 'PR Head'),
-		('Joint PR', 'Joint PR'),
+		('Joint PR Head', 'Joint PR Head'),
 		('Creative Head', 'Creative Head'),
-		('Joint Creative', 'Joint Creative'),
+		('Joint Creative Head', 'Joint Creative Head'),
 		('Technical Head', 'Technical Head'),
-		('Joint Technical', 'Joint Technical'),
+		('Joint Technical Head', 'Joint Technical Head'),
 		('Organizing Head', 'Organizing Head'),
-		('Joint Organizing', 'Joint Organizing'),
+		('Joint Organizing Head', 'Joint Organizing Head'),
 		('Magazine Head', 'Magazine Head'),
-		('Joint Magazine', 'Joint Magazine'),
+		('Joint Magazine Head', 'Joint Magazine Head'),
 		('Treasurer', 'Treasurer'),
-		('Membership', 'Membership')
+		('Joint Treasurer', 'Joint Treasurer')
 	)
 	Designation = models.CharField(max_length=50, choices=choices, default='')
 
 	def __str__(self):
-		return self.Name
+		return self.Designation + '-' + self.Name
--- Committie/views.py
+++ Committie/views.py
@@ -4,34 +4,35 @@ from .models import Committie
 
 # Created views here.
 def members(request):
-	faculty = Committie.objects.filter(Category='Faculty')
-	president = Committie.objects.filter(Designation='President')
-	vice_president = Committie.objects.filter(Designation='Vice President')
+	faculty = Committie.objects.filter(Category='Faculty').order_by('Designation')
+	president = Committie.objects.filter(Designation='President')[: 1]
+	vice_president = Committie.objects.filter(Designation='Vice President')[: 1]
 	secretary = Committie.objects.filter(Designation='Secretary')
 	joint_secretary = Committie.objects.filter(Designation='Joint Secretary')
 	sponsorship = Committie.objects.filter(Designation='Sponsorship Head')
-	joint_sponsorship = Committie.objects.filter(Designation='Joint Sponsorship')
+	joint_sponsorship = Committie.objects.filter(Designation='Joint Sponsorship Head')
 	pr = Committie.objects.filter(Designation='PR Head')
-	joint_pr = Committie.objects.filter(Designation='Joint PR')
+	joint_pr = Committie.objects.filter(Designation='Joint PR Head')
 	technical = Committie.objects.filter(Designation='Technical Head')
-	joint_technical = Committie.objects.filter(Designation='Joint Technical')
+	joint_technical = Committie.objects.filter(Designation='Joint Technical Head')
 	creative = Committie.objects.filter(Designation='Creative Head')
-	joint_creative = Committie.objects.filter(Designation='Joint Creative')
+	joint_creative = Committie.objects.filter(Designation='Joint Creative Head')
 	organizing = Committie.objects.filter(Designation='Organizing Head')
-	joint_organizing = Committie.objects.filter(Designation='Joint Organizing')
+	joint_organizing = Committie.objects.filter(Designation='Joint Organizing Head')
 	magazine = Committie.objects.filter(Designation='Magazine Head')
-	joint_magazine = Committie.objects.filter(Designation='Joint Magazine')
+	joint_magazine = Committie.objects.filter(Designation='Joint Magazine Head')
 	treasurer = Committie.objects.filter(Designation='Treasurer')
-	membership = Committie.objects.filter(Designation='Membership')
+	joint_treasurer = Committie.objects.filter(Designation='Joint Treasurer')
 	dictionary = {
 		'faculties': faculty,
 		'president': president,
 		'vice_president': vice_president,
 		'secretary': secretary,
-		'joint_sceretary': joint_secretary,
+		'joint_secretary': joint_secretary,
 		'sponsorship': sponsorship,
 		'joint_sponsorship': joint_sponsorship,
-		'pr': pr, 'joint_pr': joint_pr,
+		'pr': pr,
+		'joint_pr': joint_pr,
 		'technical': technical,
 		'joint_technical': joint_technical,
 		'creative': creative,
@@ -41,5 +42,6 @@ def members(request):
 		'magazine': magazine,
 		'joint_magazine': joint_magazine,
 		'treasurer': treasurer,
-		'membership': membership}
+		'joint_treasurer': joint_treasurer,
+	}
 	return render(request, r'committie\members.html', dictionary)
